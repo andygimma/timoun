6d428d55feeac6d719f3bc2f635b973c1fd1964a
andygimma <andy.n.gimma@gmail.com>
2015-01-10 04:57:47 -0500

    check for MSIE 8

--- handlers/LegacyHandler.py
+++ handlers/LegacyHandler.py
@@ -9,7 +9,7 @@ class LegacyHandler(webapp2.RequestHandler):
     legacy_param = self.request.params.get("legacy")
     logging.info(USER_AGENT)
     #legacy_param = False
-    if legacy_param or "MSIE 6" in USER_AGENT or "MSIE 7" in USER_AGENT:
+    if legacy_param or "MSIE 6" in USER_AGENT or "MSIE 7" in USER_AGENT or "MSIE 8" in USER_AGENT:
       self.legacy = True
     else:
       self.legacy = False
