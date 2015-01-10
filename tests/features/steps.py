61a09af210339a3102a56f9bb76a4c0eb5f0543e
andygimma <andy.n.gimma@gmail.com>
2015-01-10 02:54:50 -0500

    More complete lettuce tests

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -6,3 +6,8 @@ def when_i_click_on_group1(step, keyword):
     world.browser.find_element_by_link_text(keyword).click()
     world.browser.implicitly_wait(2)
     
+@step(u'When I click on the "([^"]*)" breadcrumb')
+def when_i_click_on_the_group1_breadcrumb(step, keyword):
+    world.browser.find_element_by_id("breadcrumb_" + str(keyword)).click()
+    world.browser.implicitly_wait(5)
+
