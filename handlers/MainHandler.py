13c053bdccc994bbd5d4c67e0fb13657ad531846
andygimma <andy.n.gimma@gmail.com>
2015-01-10 03:06:26 -0500

    Legacy check created on main handler

--- handlers/MainHandler.py
+++ handlers/MainHandler.py
@@ -2,6 +2,7 @@ import os
 import jinja2
 import webapp2
 import logging
+import re
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname( __file__ ), '..', 'templates')))
@@ -11,6 +12,13 @@ LEGACY_TEMPLATE = JINJA_ENVIRONMENT.get_template('index.html')
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
-    logging.info(self.request.headers['User-Agent'])
+    USER_AGENT = self.request.headers['User-Agent']
+    legacy = self.request.params.get("legacy")
+    logging.info(USER_AGENT)
     template_values = {}
-    self.response.write(LEGACY_TEMPLATE.render(template_values))
+    legacy = False
+    if legacy or "MSIE 6" in USER_AGENT or "MSIE 7" in self.request.headers["User-Agent"] or "MSIE 8" in self.request.headers["User-Agent"]:
+      template = LEGACY_TEMPLATE
+    else:
+      template = TEMPLATE
+    self.response.write(template.render(template_values))
