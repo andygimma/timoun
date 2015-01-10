0e6b1dd0c7cbdce6404863a08f217118f6bf3565
andygimma <andy.n.gimma@gmail.com>
2015-01-10 00:57:13 -0500

    MinCSS and static page handlers - add secure handlers (SSL on appspot domain)

--- handlers/ServicesHandler.py
+++ handlers/ServicesHandler.py
@@ -1,16 +1,14 @@
 import os
 import jinja2
 import webapp2
-import logging
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname( __file__ ), '..', 'templates')))
 
 TEMPLATE = JINJA_ENVIRONMENT.get_template('base.html')
-LEGACY_TEMPLATE = JINJA_ENVIRONMENT.get_template('index.html')
+LEGACY_TEMPLATE = JINJA_ENVIRONMENT.get_template('services.html')
 
-class MainHandler(webapp2.RequestHandler):
+class ServicesHandler(webapp2.RequestHandler):
   def get(self):
-    logging.info(self.request.headers['User-Agent'])
     template_values = {}
     self.response.write(LEGACY_TEMPLATE.render(template_values))
