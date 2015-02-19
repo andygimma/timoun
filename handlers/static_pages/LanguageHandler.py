b865c7e092b9fc5a0c1cfa286f0ce78ce7fb6b91
andygimma <andy.n.gimma@gmail.com>
2015-02-18 20:54:01 -0800

    search translation link

--- handlers/static_pages/LanguageHandler.py
+++ handlers/static_pages/LanguageHandler.py
@@ -5,11 +5,10 @@ class LanguageHandler(BaseHandler.BaseHandler):
   def get(self):
     language = self.request.get("language")
     self.response.set_cookie("language", language)
-    redirect = self.request.get("redirect")
+    redirect = self.request.get("redirect").replace("'", "")
     if redirect == "/":
-      raise Exception(1)
       self.redirect("/")
+      return
     if redirect == "/search":
-      raise Exception(2)
       self.redirect("/search")
-    raise Exception(redirect)
+      return
