72eb50009dbe9fdc7379d85170cacdfb90d53311
andygimma <andy.n.gimma@gmail.com>
2015-02-18 20:42:15 -0800

    redirect translation links

--- handlers/static_pages/LanguageHandler.py
+++ handlers/static_pages/LanguageHandler.py
@@ -5,4 +5,11 @@ class LanguageHandler(BaseHandler.BaseHandler):
   def get(self):
     language = self.request.get("language")
     self.response.set_cookie("language", language)
-    self.redirect("/")
+    redirect = self.request.get("redirect")
+    if redirect == "/":
+      raise Exception(1)
+      self.redirect("/")
+    if redirect == "/search":
+      raise Exception(2)
+      self.redirect("/search")
+    raise Exception(redirect)
