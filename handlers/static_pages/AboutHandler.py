7723bfcde0f8ef1e194bee4761322ab4060f22d9
andygimma <andy.n.gimma@gmail.com>
2015-03-09 17:26:56 -0700

    translated pages added

--- handlers/static_pages/AboutHandler.py
+++ handlers/static_pages/AboutHandler.py
@@ -8,13 +8,9 @@ JINJA_ENVIRONMENT = jinja2.Environment(
         [os.path.join(os.path.dirname(__file__),"../../templates/static_pages"),
          os.path.join(os.path.dirname(__file__),"../../templates/layouts")]))
 
-LEGACY_TEMPLATE = JINJA_ENVIRONMENT.get_template('about.html')
 
 class AboutHandler(BaseHandler.BaseHandler):
   def get(self):
-    if not self.legacy:
-      self.redirect("/#/about")
-      return
     role = self.session.get('role')
     user_session = self.session.get("user")
     template_values = {
@@ -22,4 +18,17 @@ class AboutHandler(BaseHandler.BaseHandler):
       "user_session": user_session,
       "message": self.request.get("message")
     }
+    language = None
+    if "language" in self.request.cookies:
+      language = self.request.cookies["language"]
+    else:
+      language = "fr"
+      self.response.set_cookie("language", "fr")
+
+    language = language.replace('"', '').replace("'", "")
+    if language == "fr":
+
+      LEGACY_TEMPLATE = JINJA_ENVIRONMENT.get_template('fr_about.html')
+    else:
+      LEGACY_TEMPLATE = JINJA_ENVIRONMENT.get_template('en_about.html')
     self.response.write(LEGACY_TEMPLATE.render(template_values))
--- handlers/static_pages/SuggestServicesHandler.py
+++ handlers/static_pages/SuggestServicesHandler.py
@@ -8,7 +8,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
         [os.path.join(os.path.dirname(__file__),"../../templates/static_pages"),
          os.path.join(os.path.dirname(__file__),"../../templates/layouts")]))
 
-LEGACY_TEMPLATE = JINJA_ENVIRONMENT.get_template('suggest_services.html')
 
 class SuggestServicesHandler(BaseHandler.BaseHandler):
   def get(self):
@@ -21,4 +20,19 @@ class SuggestServicesHandler(BaseHandler.BaseHandler):
       "user_session": user_session,
       "message": self.request.get("message")
     }
+
+    language = None
+    if "language" in self.request.cookies:
+      language = self.request.cookies["language"]
+    else:
+      language = "fr"
+      self.response.set_cookie("language", "fr")
+
+    language = language.replace('"', '').replace("'", "")
+    if language == "fr":
+
+      LEGACY_TEMPLATE = JINJA_ENVIRONMENT.get_template('fr_suggest_services.html')
+    else:
+      LEGACY_TEMPLATE = JINJA_ENVIRONMENT.get_template('en_suggest_services.html')
+
     self.response.write(LEGACY_TEMPLATE.render(template_values))
